be605c14839953f230f6ac1f9f9756ca1e9e3a0e
knightc <knightc@kc-dev-3759.local>
2026-01-16 11:03:48 +0800

    fix(sha256): Fix 19 compilation errors - mark unused vars + sign-conversion casts
    - Line 149: Mark t1, t2 as [[maybe_unused]] in transform_scalar()
    - Lines 214,222,230,238,250,260,270,280,290,300,310,320,330,340,349,358:
      Cast all ULL constants to int64_t in _mm_set_epi64x() calls
      Fixes 16 sign-conversion warnings from SIMD intrinsics
    
    All 19 errors resolved: File now compiles cleanly with -Werror

--- src/crypto/sha256.cpp
+++ src/crypto/sha256.cpp
@@ -146,7 +146,7 @@ public:
     static void transform_scalar(kctsb_sha256_ctx_t* ctx, const uint8_t block[64]) noexcept {
         uint32_t W[64];
         uint32_t a, b, c, d, e, f, g, h;
-        uint32_t t1, t2;
+        uint32_t t1 [[maybe_unused]], t2 [[maybe_unused]];
 
         // Prepare message schedule (load + expand)
         for (size_t i = 0; i < 16; i++) {
@@ -211,7 +211,7 @@ public:
         // Rounds 0-3
         MSG = _mm_loadu_si128(reinterpret_cast<const __m128i*>(block + 0));
         MSG0 = _mm_shuffle_epi8(MSG, MASK);
-        MSG = _mm_add_epi32(MSG0, _mm_set_epi64x(0xE9B5DBA5B5C0FBCFULL, 0x71374491428A2F98ULL));
+        MSG = _mm_add_epi32(MSG0, _mm_set_epi64x(static_cast<int64_t>(0xE9B5DBA5B5C0FBCFULL), static_cast<int64_t>(0x71374491428A2F98ULL)));
         STATE1 = _mm_sha256rnds2_epu32(STATE1, STATE0, MSG);
         MSG = _mm_shuffle_epi32(MSG, 0x0E);
         STATE0 = _mm_sha256rnds2_epu32(STATE0, STATE1, MSG);
@@ -219,7 +219,7 @@ public:
         // Rounds 4-7
         MSG1 = _mm_loadu_si128(reinterpret_cast<const __m128i*>(block + 16));
         MSG1 = _mm_shuffle_epi8(MSG1, MASK);
-        MSG = _mm_add_epi32(MSG1, _mm_set_epi64x(0xAB1C5ED5923F82A4ULL, 0x59F111F13956C25BULL));
+        MSG = _mm_add_epi32(MSG1, _mm_set_epi64x(static_cast<int64_t>(0xAB1C5ED5923F82A4ULL), static_cast<int64_t>(0x59F111F13956C25BULL)));
         STATE1 = _mm_sha256rnds2_epu32(STATE1, STATE0, MSG);
         MSG = _mm_shuffle_epi32(MSG, 0x0E);
         STATE0 = _mm_sha256rnds2_epu32(STATE0, STATE1, MSG);
@@ -228,7 +228,7 @@ public:
         // Rounds 8-11
         MSG2 = _mm_loadu_si128(reinterpret_cast<const __m128i*>(block + 32));
         MSG2 = _mm_shuffle_epi8(MSG2, MASK);
-        MSG = _mm_add_epi32(MSG2, _mm_set_epi64x(0x550C7DC3243185BEULL, 0x12835B01D807AA98ULL));
+        MSG = _mm_add_epi32(MSG2, _mm_set_epi64x(static_cast<int64_t>(0x550C7DC3243185BEULL), static_cast<int64_t>(0x12835B01D807AA98ULL)));
         STATE1 = _mm_sha256rnds2_epu32(STATE1, STATE0, MSG);
         MSG = _mm_shuffle_epi32(MSG, 0x0E);
         STATE0 = _mm_sha256rnds2_epu32(STATE0, STATE1, MSG);
@@ -237,7 +237,7 @@ public:
         // Rounds 12-15
         MSG3 = _mm_loadu_si128(reinterpret_cast<const __m128i*>(block + 48));
         MSG3 = _mm_shuffle_epi8(MSG3, MASK);
-        MSG = _mm_add_epi32(MSG3, _mm_set_epi64x(0xC19BF1749BDC06A7ULL, 0x80DEB1FE72BE5D74ULL));
+        MSG = _mm_add_epi32(MSG3, _mm_set_epi64x(static_cast<int64_t>(0xC19BF1749BDC06A7ULL), static_cast<int64_t>(0x80DEB1FE72BE5D74ULL)));
         STATE1 = _mm_sha256rnds2_epu32(STATE1, STATE0, MSG);
         TMP = _mm_alignr_epi8(MSG3, MSG2, 4);
         MSG0 = _mm_add_epi32(MSG0, TMP);
@@ -247,7 +247,7 @@ public:
         MSG2 = _mm_sha256msg1_epu32(MSG2, MSG3);
         
         // Rounds 16-19
-        MSG = _mm_add_epi32(MSG0, _mm_set_epi64x(0x240CA1CC0FC19DC6ULL, 0xEFBE4786E49B69C1ULL));
+        MSG = _mm_add_epi32(MSG0, _mm_set_epi64x(static_cast<int64_t>(0x240CA1CC0FC19DC6ULL), static_cast<int64_t>(0xEFBE4786E49B69C1ULL)));
         STATE1 = _mm_sha256rnds2_epu32(STATE1, STATE0, MSG);
         TMP = _mm_alignr_epi8(MSG0, MSG3, 4);
         MSG1 = _mm_add_epi32(MSG1, TMP);
@@ -257,7 +257,7 @@ public:
         MSG3 = _mm_sha256msg1_epu32(MSG3, MSG0);
         
         // Rounds 20-23
-        MSG = _mm_add_epi32(MSG1, _mm_set_epi64x(0x76F988DA5CB0A9DCULL, 0x4A7484AA2DE92C6FULL));
+        MSG = _mm_add_epi32(MSG1, _mm_set_epi64x(static_cast<int64_t>(0x76F988DA5CB0A9DCULL), static_cast<int64_t>(0x4A7484AA2DE92C6FULL)));
         STATE1 = _mm_sha256rnds2_epu32(STATE1, STATE0, MSG);
         TMP = _mm_alignr_epi8(MSG1, MSG0, 4);
         MSG2 = _mm_add_epi32(MSG2, TMP);
@@ -267,7 +267,7 @@ public:
         MSG0 = _mm_sha256msg1_epu32(MSG0, MSG1);
         
         // Rounds 24-27
-        MSG = _mm_add_epi32(MSG2, _mm_set_epi64x(0xBF597FC7B00327C8ULL, 0xA831C66D983E5152ULL));
+        MSG = _mm_add_epi32(MSG2, _mm_set_epi64x(static_cast<int64_t>(0xBF597FC7B00327C8ULL), static_cast<int64_t>(0xA831C66D983E5152ULL)));
         STATE1 = _mm_sha256rnds2_epu32(STATE1, STATE0, MSG);
         TMP = _mm_alignr_epi8(MSG2, MSG1, 4);
         MSG3 = _mm_add_epi32(MSG3, TMP);
@@ -277,7 +277,7 @@ public:
         MSG1 = _mm_sha256msg1_epu32(MSG1, MSG2);
         
         // Rounds 28-31
-        MSG = _mm_add_epi32(MSG3, _mm_set_epi64x(0x1429296706CA6351ULL, 0xD5A79147C6E00BF3ULL));
+        MSG = _mm_add_epi32(MSG3, _mm_set_epi64x(static_cast<int64_t>(0x1429296706CA6351ULL), static_cast<int64_t>(0xD5A79147C6E00BF3ULL)));
         STATE1 = _mm_sha256rnds2_epu32(STATE1, STATE0, MSG);
         TMP = _mm_alignr_epi8(MSG3, MSG2, 4);
         MSG0 = _mm_add_epi32(MSG0, TMP);
@@ -287,7 +287,7 @@ public:
         MSG2 = _mm_sha256msg1_epu32(MSG2, MSG3);
         
         // Rounds 32-35
-        MSG = _mm_add_epi32(MSG0, _mm_set_epi64x(0x53380D134D2C6DFCULL, 0x2E1B213827B70A85ULL));
+        MSG = _mm_add_epi32(MSG0, _mm_set_epi64x(static_cast<int64_t>(0x53380D134D2C6DFCULL), static_cast<int64_t>(0x2E1B213827B70A85ULL)));
         STATE1 = _mm_sha256rnds2_epu32(STATE1, STATE0, MSG);
         TMP = _mm_alignr_epi8(MSG0, MSG3, 4);
         MSG1 = _mm_add_epi32(MSG1, TMP);
@@ -297,7 +297,7 @@ public:
         MSG3 = _mm_sha256msg1_epu32(MSG3, MSG0);
         
         // Rounds 36-39
-        MSG = _mm_add_epi32(MSG1, _mm_set_epi64x(0x92722C8581C2C92EULL, 0x766A0ABB650A7354ULL));
+        MSG = _mm_add_epi32(MSG1, _mm_set_epi64x(static_cast<int64_t>(0x92722C8581C2C92EULL), static_cast<int64_t>(0x766A0ABB650A7354ULL)));
         STATE1 = _mm_sha256rnds2_epu32(STATE1, STATE0, MSG);
         TMP = _mm_alignr_epi8(MSG1, MSG0, 4);
         MSG2 = _mm_add_epi32(MSG2, TMP);
@@ -307,7 +307,7 @@ public:
         MSG0 = _mm_sha256msg1_epu32(MSG0, MSG1);
         
         // Rounds 40-43
-        MSG = _mm_add_epi32(MSG2, _mm_set_epi64x(0xC76C51A3C24B8B70ULL, 0xA81A664BA2BFE8A1ULL));
+        MSG = _mm_add_epi32(MSG2, _mm_set_epi64x(static_cast<int64_t>(0xC76C51A3C24B8B70ULL), static_cast<int64_t>(0xA81A664BA2BFE8A1ULL)));
         STATE1 = _mm_sha256rnds2_epu32(STATE1, STATE0, MSG);
         TMP = _mm_alignr_epi8(MSG2, MSG1, 4);
         MSG3 = _mm_add_epi32(MSG3, TMP);
@@ -317,7 +317,7 @@ public:
         MSG1 = _mm_sha256msg1_epu32(MSG1, MSG2);
         
         // Rounds 44-47
-        MSG = _mm_add_epi32(MSG3, _mm_set_epi64x(0x106AA070F40E3585ULL, 0xD6990624D192E819ULL));
+        MSG = _mm_add_epi32(MSG3, _mm_set_epi64x(static_cast<int64_t>(0x106AA070F40E3585ULL), static_cast<int64_t>(0xD6990624D192E819ULL)));
         STATE1 = _mm_sha256rnds2_epu32(STATE1, STATE0, MSG);
         TMP = _mm_alignr_epi8(MSG3, MSG2, 4);
         MSG0 = _mm_add_epi32(MSG0, TMP);
@@ -327,7 +327,7 @@ public:
         MSG2 = _mm_sha256msg1_epu32(MSG2, MSG3);
         
         // Rounds 48-51
-        MSG = _mm_add_epi32(MSG0, _mm_set_epi64x(0x34B0BCB52748774CULL, 0x1E376C0819A4C116ULL));
+        MSG = _mm_add_epi32(MSG0, _mm_set_epi64x(static_cast<int64_t>(0x34B0BCB52748774CULL), static_cast<int64_t>(0x1E376C0819A4C116ULL)));
         STATE1 = _mm_sha256rnds2_epu32(STATE1, STATE0, MSG);
         TMP = _mm_alignr_epi8(MSG0, MSG3, 4);
         MSG1 = _mm_add_epi32(MSG1, TMP);
@@ -337,7 +337,7 @@ public:
         MSG3 = _mm_sha256msg1_epu32(MSG3, MSG0);
         
         // Rounds 52-55
-        MSG = _mm_add_epi32(MSG1, _mm_set_epi64x(0x682E6FF35B9CCA4FULL, 0x4ED8AA4A391C0CB3ULL));
+        MSG = _mm_add_epi32(MSG1, _mm_set_epi64x(static_cast<int64_t>(0x682E6FF35B9CCA4FULL), static_cast<int64_t>(0x4ED8AA4A391C0CB3ULL)));
         STATE1 = _mm_sha256rnds2_epu32(STATE1, STATE0, MSG);
         TMP = _mm_alignr_epi8(MSG1, MSG0, 4);
         MSG2 = _mm_add_epi32(MSG2, TMP);
@@ -346,7 +346,7 @@ public:
         STATE0 = _mm_sha256rnds2_epu32(STATE0, STATE1, MSG);
         
         // Rounds 56-59
-        MSG = _mm_add_epi32(MSG2, _mm_set_epi64x(0x8CC7020884C87814ULL, 0x78A5636F748F82EEULL));
+        MSG = _mm_add_epi32(MSG2, _mm_set_epi64x(static_cast<int64_t>(0x8CC7020884C87814ULL), static_cast<int64_t>(0x78A5636F748F82EEULL)));
         STATE1 = _mm_sha256rnds2_epu32(STATE1, STATE0, MSG);
         TMP = _mm_alignr_epi8(MSG2, MSG1, 4);
         MSG3 = _mm_add_epi32(MSG3, TMP);
@@ -355,7 +355,7 @@ public:
         STATE0 = _mm_sha256rnds2_epu32(STATE0, STATE1, MSG);
         
         // Rounds 60-63
-        MSG = _mm_add_epi32(MSG3, _mm_set_epi64x(0xC67178F2BEF9A3F7ULL, 0xA4506CEB90BEFFFAULL));
+        MSG = _mm_add_epi32(MSG3, _mm_set_epi64x(static_cast<int64_t>(0xC67178F2BEF9A3F7ULL), static_cast<int64_t>(0xA4506CEB90BEFFFAULL)));
         STATE1 = _mm_sha256rnds2_epu32(STATE1, STATE0, MSG);
         MSG = _mm_shuffle_epi32(MSG, 0x0E);
         STATE0 = _mm_sha256rnds2_epu32(STATE0, STATE1, MSG);
